207d3ac3f22548b1df19b49778e759852e6e2d2c
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2025-11-14 12:44:20 -0500

    Added Class Variables to Program class

--- ICE9A/Program.cs
+++ ICE9A/Program.cs
@@ -1,11 +1,36 @@
 namespace ICE9A
 {
+    public enum FormType
+    {
+        Splash,
+        Start,
+        Next,
+        Final,
+        About
+    }
+
     internal static class Program
     {
+        // Declaring Form Variables
+        public static SplashForm SplashForm;
+        public static SelectionForm SelectionForm;
+        public static NextForm NextForm;
+        public static FinalForm FinalForm;
+        public static AboutForm AboutForm;
+
+        // Declare List of Form type named Forms
+        public static List<Form> Forms;
+
+        // Declare and initialize a bool named IsExiting
+        public static bool IsExiting = false;
+
+
         [STAThread]
         static void Main()
         {
             ApplicationConfiguration.Initialize();
+
+            // Instantiate an object of type SelectionForm
             Application.Run(new SelectionForm());
         }
     }
